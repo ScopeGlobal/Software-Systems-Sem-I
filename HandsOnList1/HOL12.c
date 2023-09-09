1377fe5c0de1d11362e8874f85d251906f303fcd
ScopeGlobal <itssridhar2001@gmail.com>
2023-09-09 14:08:10 +0530

    Made corrections in HOL 1 Q. 12 and 14

--- HandsOnList1/HOL12.c
+++ HandsOnList1/HOL12.c
@@ -17,7 +17,15 @@ int main(void) {
 	if (fd < 0) {
 		perror("File opening failed ");
 	}
-	printf("F_GETFD %d", fcntl(fd, F_GETFD));
-	printf("F_GETFL %d", fcntl(fd, F_GETFL));
+	printf("F_GETFD %d \n", fcntl(fd, F_GETFD));
+	printf("F_GETFL %d \n", fcntl(fd, F_GETFL));
+	
+	switch (fcntl(fd, F_GETFL)) {
+		case 32770: printf("O_RDWR"); break;
+		case 32768: printf("O_RDONLY"); break;
+		case 32769: printf("O_WRONLY"); break;
+		default: printf("Invalid");
+	
+	}
 	return 0;
 }
--- HandsOnList1/HOL14.c
+++ HandsOnList1/HOL14.c
@@ -32,5 +32,15 @@ int main(int argc, char* argv[]) {
 	}
 	
 	printf("File type: %d", st.st_mode);
+	switch (st.st_mode & S_IFMT) {
+           case S_IFBLK:  printf("block device\n");            break;
+           case S_IFCHR:  printf("character device\n");        break;
+           case S_IFDIR:  printf("directory\n");               break;
+           case S_IFIFO:  printf("FIFO/pipe\n");               break;
+           case S_IFLNK:  printf("symlink\n");                 break;
+           case S_IFREG:  printf("regular file\n");            break;
+           case S_IFSOCK: printf("socket\n");                  break;
+           default:       printf("unknown?\n");                break;
+           }
 	return 0;
 }
